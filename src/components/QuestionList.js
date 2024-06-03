a38292b6835bfbaa8efcb08f7e6c08967e0d1fa4
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-03 12:43:18 +0300

    feat: Add support for adding new questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,20 +1,19 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect} from "react";
 import QuestionItem from './QuestionItem'
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([])
+function QuestionList({questions, setQuestions}) {
 
   useEffect(() => {
     fetch('http://localhost:4000/questions')
       .then(response => response.json())
       .then(data => setQuestions(data))
       .catch(error => console.log(`Error fetching questions: ${error}`))
-  }, [])
+  },[])
 
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{questions.map(question => <QuestionItem question={question}/>
+      <ul>{questions.map(question => <QuestionItem key={question.id} question={question}/>
       )}</ul>
     </section>
   );
